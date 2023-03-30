e08b31ec56d5ef3584185dd3b0e95f7619e8666f
Parker Fink <parke2023059@live.ytech.edu>
2023-03-30 14:25:57 -0400

    attemmp 11 finish

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+var port = 8080
+
+
+function round( value ){
+
+    value *= 100
+    value = Math.round(value)
+    value /= 100
+
+    return value 
+
+}
+
+class Item{
+    constructor(name, qty, unitprice){
+        this.name = name
+        this.qty = qty
+        this.unitprice = unitprice
+        this.profit = unitprice * 0.4
+        this.retailprice = this.profit + unitprice
+        this.subtotal = this.retailprice * qty
+        this.salestax = this.subtotal * 0.06
+
+    }
+}
+
+var orders = []
+
+app.get('/', function(req,res){
+    res.render('index.ejs')
+})
+
+app.post('/', function(req,res){
+
+    var order = {
+        items: [],
+        name: req.body.name,
+        address: req.body.address,
+        shipping: 0,
+        subtotal: 0,
+        salestax: 0,
+        profit: 0,
+        total: 0
+    }
+
+    if(req.body.item1) {order.items.push(new Item("Battlepass", req.body.item1, 7.13))}
+    if(req.body.item2) {order.items.push(new Item("Card", req.body.item2, 14.28))}
+    if(req.body.item3) {order.items.push(new Item("DLC", req.body.item3, 48.78))}
+
+    if(order.shipping < 40) {order.shipping = 15}
+    else if (order.shipping < 150) {order.shipping = 10}
+
+    for (let item of order.items){
+        order.subtotal = round(item.subtotal)
+        order.salestax = round(item.salestax)
+        order.profit = round(item.profit)
+
+
+
+        item.subtotal = round(item.subtotal)
+        item.salestax = round(item.salestax)
+        item.profit = round(item.profit)
+        item.retailprice = round(item.retailprice)
+
+    
+        order.subtotal += item.subtotal
+        order.salestax += item.salestax
+        order.profit += order.salestax
+    }
+
+    order.total = order.subtotal + order.shipping + order.salestax
+    order.total = round(order.total)
+
+
+    orders.push(order)
+    console.log(orders)
+    res.redirect('/viewall')
+})
+
+app.get('/viewall', function(req,res){
+    res.render('viewall.ejs', {
+        orders: orders
+    })
+})
+
+
+app.listen(port, function(){
+    console.log(`Server running on port ${port}`)
+})
